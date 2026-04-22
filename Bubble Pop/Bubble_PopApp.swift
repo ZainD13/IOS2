a322b94c3335e6c7305ff59f5d52805949a912e0
Damaq Zain <zaindamaq@gmail.com>
2026-04-22 02:57:07 -0700

    Initial Commit

---
+++ Bubble Pop/Bubble_PopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_PopApp.swift
+//  Bubble Pop
+//
+//  Created by rentamac on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Bubble_PopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bubble Pop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bubble Pop
+//
+//  Created by rentamac on 4/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
